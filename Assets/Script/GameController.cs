0d7eee4e5983146739a6df20bb7b6d727523a16f
apw <anggitprayogo@gmail.com>
2017-10-07 05:54:26 +0700

    initial commit guess the number

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+g
+public class GameController : MonoBehaviour {
+
+	public InputField guessingNumber;
+	public Text guessedNumber;
+	private int guessnumber;
+	private int userNumber;
+
+	// Use this for initialization
+	void Start () {
+		guessnumber = Random.Range (1, 100);
+
+	}
+
+	public void checkGuess(){
+		userNumber = int.Parse(guessingNumber.text);
+		if(userNumber == guessnumber){
+			guessedNumber.text = "Keren! Tebakan mu benar..";
+		}else if(userNumber > guessnumber){
+			guessedNumber.text = "Kegedean tabakannya";
+		}else if(userNumber < guessnumber){
+			guessedNumber.text = "Kekecilan tebakannya";
+		}
+
+		guessingNumber.text = "";
+	}
+}
